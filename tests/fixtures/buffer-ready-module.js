edd50f03f61ad2fae3b1d88a5eed15c58afdbd27
slonka <slonka@users.noreply.github.com>
2018-07-13 13:27:46 +0200

    use experimental worker_threads to run tasks

--- tests/fixtures/buffer-ready-module.js
+++ tests/fixtures/buffer-ready-module.js
@@ -3,6 +3,6 @@ const zlib = require('zlib');
 module.exports = {
     getDeflated: (msg = 'Hello!') => zlib.deflateSync(msg),
     getDeflatedInProperty: (msg = 'Hello!') => ({ info: 'deflate', data: zlib.deflateSync(msg) }),
-    isBuffer: arg => Buffer.isBuffer(arg),
+    isUint8Array: arg => arg instanceof Uint8Array,
     sliceInHalf: buffer => buffer.slice(0, Math.floor(buffer.length / 2))
 };
