1c28b1a7896a64442c9337db4ce54e9f550ee340
Yury Michurin <yurym@wix.com>
2023-06-18 21:16:24 +0300

    Refine the test to better assert expectations

--- e2e/spawn-new-workers-on-stop.spec.js
+++ e2e/spawn-new-workers-on-stop.spec.js
@@ -6,21 +6,24 @@ const { fixture, repeatCall, eventually } = require('./utils');
 for (const workerType of ["thread", "process"]) {
     test(`should spawn new workers when old workers exit even if no items in the queue workerType: ${workerType}`, async (t) => {
         // given
+        const getOperationalWorkersCount = () => workerNodes.workersQueue.filter((worker) => worker.isOperational()).length;
         const maxWorkers = 4;
         const minWorkers = 2;
         const workerNodes = new WorkerNodes(fixture('process-info'), { maxWorkers, minWorkers, workerEndurance: 1, autoStart: true, workerType });
 
         await workerNodes.ready();
 
-        const operationWorkersCountBefore = workerNodes.workersQueue.filter((worker) => worker.isOperational()).length;
-        t.is(operationWorkersCountBefore, minWorkers);
+        t.is(getOperationalWorkersCount(), minWorkers);
+        const workerIdsBeforeTaskExecution = new Set(workerNodes.workersQueue.map(worker => worker.id));
 
         // when
         await repeatCall(workerNodes.call.noop, maxWorkers);
-        t.is(workerNodes.workersQueue.filter((worker) => worker.isOperational()).length, 0);
+        
+        // we expect the workers we used to be released
+        const getOperationWorkersWithIdsCount = (ids) => workerNodes.workersQueue.filter((worker) => ids.has(worker.id)).length;
+        await eventually(() => getOperationWorkersWithIdsCount(workerIdsBeforeTaskExecution) === 0)
+        t.is(getOperationWorkersWithIdsCount(workerIdsBeforeTaskExecution), 0);
 
-
-        const getOperationalWorkersCount = () => workerNodes.workersQueue.filter((worker) => worker.isOperational()).length;
 
         // then
         // we're waiting to all workers to be operational, the time for that might variate greatly between machines (Developer machine vs CI machine)
