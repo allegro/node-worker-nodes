5ee7a310d831da954b55739a84d7dc37d66e4f68
Yury Michurin <yurym@wix.com>
2023-06-19 09:34:25 +0300

    Test raw data transfer on process as well

--- e2e/raw-data.spec.js
+++ e2e/raw-data.spec.js
@@ -3,50 +3,52 @@ const test = require('ava');
 const WorkerNodes = require('../');
 const { fixture } = require('./utils');
 
-test('should allow to receive a Uint8Array from the worker', async (t) => {
-    // given
-    const workerNodes = new WorkerNodes(fixture('buffer-ready-module'), { workerType: "thread" });
+for (const workerType of ["thread", "process"]) {
+    test(`should allow to receive a Uint8Array from the worker workerType: ${workerType}`, async (t) => {
+        // given
+        const workerNodes = new WorkerNodes(fixture('buffer-ready-module'), { workerType });
 
-    // when
-    const result = await workerNodes.call.getDeflated('hello my friend!!!');
-    const decoded = require('zlib').inflateSync(result).toString();
+        // when
+        const result = await workerNodes.call.getDeflated('hello my friend!!!');
+        const decoded = require('zlib').inflateSync(result).toString();
 
-    // then
-    t.true(result instanceof Uint8Array);
-    t.is(decoded, 'hello my friend!!!');
-});
+        // then
+        t.true(result instanceof Uint8Array);
+        t.is(decoded, 'hello my friend!!!');
+    });
 
-test('should allow to receive a Uint8Array as an object property', async (t) => {
-    // given
-    const workerNodes = new WorkerNodes(fixture('buffer-ready-module'), { workerType: "thread" });
+    test(`should allow to receive a Uint8Array as an object property workerType: ${workerType}`, async (t) => {
+        // given
+        const workerNodes = new WorkerNodes(fixture('buffer-ready-module'), { workerType });
 
-    // when
-    const result = await workerNodes.call.getDeflatedInProperty('hello my friend!!!');
+        // when
+        const result = await workerNodes.call.getDeflatedInProperty('hello my friend!!!');
 
-    // then
-    t.is(result.info, 'deflate');
-    t.true(result.data instanceof Uint8Array);
-    t.is(require('zlib').inflateSync(result.data).toString(), 'hello my friend!!!')
-});
+        // then
+        t.is(result.info, 'deflate');
+        t.true(result.data instanceof Uint8Array);
+        t.is(require('zlib').inflateSync(result.data).toString(), 'hello my friend!!!')
+    });
 
-test('should allow to send a Uint8Array to the worker', async (t) => {
-    // given
-    const workerNodes = new WorkerNodes(fixture('buffer-ready-module'), { workerType: "thread" });
+    test(`should allow to send a Uint8Array to the worker workerType: ${workerType}`, async (t) => {
+        // given
+        const workerNodes = new WorkerNodes(fixture('buffer-ready-module'), { workerType });
 
-    // when
-    const result = await workerNodes.call.isUint8Array(Buffer.from('foobar'));
+        // when
+        const result = await workerNodes.call.isUint8Array(Buffer.from('foobar'));
 
-    // then
-    t.true(result);
-});
+        // then
+        t.true(result);
+    });
 
-test('should be available in both directions', async (t) => {
-    // given
-    const workerNodes = new WorkerNodes(fixture('buffer-ready-module'), { workerType: "thread" });
+    test(`should be available in both directions workerType: ${workerType}`, async (t) => {
+        // given
+        const workerNodes = new WorkerNodes(fixture('buffer-ready-module'), { workerType });
 
-    // when
-    const result = await workerNodes.call.sliceInHalf(Buffer.from('foobar'));
+        // when
+        const result = await workerNodes.call.sliceInHalf(Buffer.from('foobar'));
 
-    // then
-    t.is(Buffer.compare(Buffer.from('foo'), result), 0);
-});
+        // then
+        t.is(Buffer.compare(Buffer.from('foo'), result), 0);
+    });
+}
